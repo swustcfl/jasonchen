190ad655d5fd77b4fc4c08c09acb876c12e5011b
jason.chen <jason.chen@justalk.com>
2018-06-04 09:45:52 +0800

    feature: 分离redguardtoo和我自己的  9e53d11
    9e53d11f3873033988a7cb5fd7a813b66ceb7a54

---
+++ lisp/init-jasonchen.el
@@ -0,0 +1,75 @@
+
+;; 为了org中表格的中英文对齐
+(defun spacemacs//set-monospaced-font (english chinese english-size chinese-size)
+  (set-face-attribute 'default nil :font
+                      (format   "%s:pixelsize=%d"  english english-size))
+  (dolist (charset '(kana han cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font) charset
+                      (font-spec :family chinese :size chinese-size))))
+;;-----------------------------org begin----------------------------------------
+;; 设置org相关
+;; 设置字体集和face的想法来自http://zcodes.net/2016/08/22/emacs-notes-config.html
+;; https://github.com/zcodes/emacs.d/blob/7d15ef5793a5f1d2f89aa3277639aa1ed89a9ff0/lisp/zcodes-org.el
+
+;;这个是单单对org的table产生影响的设置
+;;FIXBUG
+(defun jc-org-zh-en-aligned-table ()
+  (interactive)
+
+  ;;创建字体集合，ascii码使用 source code pro ，中文使用hiragino
+  (setq fontset-orgtable (create-fontset-from-ascii-font "Source Code Pro-14"))
+  (dolist (charset '(kana han cjk-misc bopomofo))
+    (set-fontset-font fontset-orgtable charset
+                      (font-spec :family  "Hiragino Sans GB" :size 16)))
+
+  ;;设置org-table的face属性
+  (set-face-attribute 'org-table nil :font
+                      (format   "%s:pixelsize=%d"  "Source Code Pro"  14)
+                      :fontset fontset-orgtable)
+  )
+
+;;生效这个org的整个buffer
+(defun jc-org-zh-en-aligned-buffer ()
+  (interactive)
+
+  ;;创建一个字体集，这个字体集合，中文字符集的话使用Hiragino Sans GB
+  (setq fontset-org (create-fontset-from-ascii-font "Source Code Pro-14"))
+  (dolist (charset '(kana han cjk-misc bopomofo))
+    (set-fontset-font fontset-org charset
+                      (font-spec :family  "Hiragino Sans GB" :size 16)))
+
+  ;;创建face，这个字体集合使用上面
+  (make-face 'jc-org-faceset)
+  (set-face-attribute 'jc-org-faceset nil :font
+                      (format   "%s:pixelsize=%d"  "Source Code Pro"  14)
+                      :fontset fontset-org)
+ 
+  ;;生效某个buffer使用特定face
+  (buffer-face-mode t)
+  (buffer-face-set 'jc-org-faceset)
+  )
+
+;;加载org生效
+(add-hook 'org-mode-hook 'jc-org-zh-en-aligned-buffer)
+;;-----------------------------org end------------------------------------------
+
+;;配置nilsdeppe
+;;安装的包需要在这里写上不然会提示包没有
+(setq  melpa-include-packages
+       (append  melpa-include-packages
+                '(clang-format
+                  modern-cpp-font-lock
+                  google-c-style
+                  magit)))
+(require-init 'init-nilsdeppe)
+
+;;---------------------------find file in project -----------------------------
+;;项目涉及很多git仓库，所以要ffip-project-root要统一到最上层
+(defun jc-project-root ()
+  (when (f-descendant-of-p (buffer-file-name) (file-truename "~/Documents/justalk/justalk"))
+      (setq-local ffip-project-root (file-truename "~/Documents/justalk/justalk")))
+  )
+(add-hook 'prog-mode-hook 'jc-project-root)
+;;---------------------------find file in project -----------------------------
+
+(provide 'init-jasonchen)
---
+++ lisp/init-nilsdeppe.el
@@ -0,0 +1,385 @@
+;------------------------------------------------------------------------------
+;;下面的内容来自https://gist.github.com /nilsdeppe/7645c096d93b005458d97d6874a91ea9
+
+;; Specify the ycmd server command and path to the ycmd directory *inside* the
+;; cloned ycmd directory
+(setq jc-ycmd-dir (file-truename "~/Documents/tools/ycmd/ycmd"))
+(defvar my:ycmd-server-command `("python" ,jc-ycmd-dir))
+(defvar my:ycmd-extra-conf-whitelist '("~/.ycm_extra_conf.py"))
+(defvar my:ycmd-global-config "~/.ycm_extra_conf.py")
+
+;; Compilation command for C/C++
+(defvar my:compile-command "clang++ -Wall -Wextra -std=c++11 ")
+
+;; Non-nil means draw block cursor as wide as the glyph under it.
+;; For example, if a block cursor is over a tab, it will be drawn as
+;; wide as that tab on the display.
+(setq x-stretch-cursor t)
+
+;;shebang指的是#！，比如#!/usr/bin/env python
+;; Check (on save) whether the file edited contains a shebang, if yes,
+;; make it executable from
+;; http://mbork.pl/2015-01-10_A_few_random_Emacs_tips
+;;(add-hook 'after-save-hook #'executable-make-buffer-file-executable-if-script-p)
+
+
+;; Highlight some keywords in prog-mode
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (font-lock-add-keywords
+             nil
+             '(("\\<\\(FIXME\\|TODO\\|BUG\\|DONE\\)" 1 font-lock-warning-face t)
+               )
+             )
+            )
+          )
+
+;; Disable package initialize after us.  We either initialize it
+;; anyway in case of interpreted .emacs, or we don't want slow
+;; initizlization in case of byte-compiled .emacs.elc.
+(setq package-enable-at-startup nil)
+;; Ask package.el to not add (package-initialize) to .emacs.
+(setq package--init-file-ensured t)
+;; set use-package-verbose to t for interpreted .emacs,
+;; and to nil for byte-compiled .emacs.elc
+(eval-and-compile
+  (setq use-package-verbose (not (bound-and-true-p byte-compile-current-file))))
+;; Add the macro generated list of package.el loadpaths to load-path.
+(mapc #'(lambda (add) (add-to-list 'load-path add))
+      (eval-when-compile
+        (require 'package)
+        (package-initialize)
+        ;; Install use-package if not installed yet.
+        (unless (package-installed-p 'use-package)
+          (package-refresh-contents)
+          (package-install 'use-package))
+        ;; (require 'use-package)
+        (let ((package-user-dir-real (file-truename package-user-dir)))
+          ;; The reverse is necessary, because outside we mapc
+          ;; add-to-list element-by-element, which reverses.
+          (nreverse (apply #'nconc
+                           ;; Only keep package.el provided loadpaths.
+                           (mapcar #'(lambda (path)
+                                       (if (string-prefix-p package-user-dir-real path)
+                                           (list path)
+                                         nil))
+                                   load-path))))))
+;; 加载use-package
+(eval-when-compile
+  (require 'use-package))
+
+(use-package bind-key)
+;; so we can (require 'use-package) even in compiled emacs to e.g. read docs
+(use-package use-package
+  :commands use-package-autoload-keymap)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; async - library for async/thread processing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package async
+  :ensure t)
+
+;;配置counsel-etags
+;;忽略目录和文件
+;;(add-to-list 'counsel-etags-ignore-directories "build_clang")
+;;(add-to-list 'counsel-etags-ignore-directories ".vscode")
+;;(add-to-list 'counsel-etags-ignore-filenames ".clang-format")
+;;(add-to-list 'counsel-etags-ignore-filenames "TAGS")
+;;(add-to-list 'counsel-etags-ignore-filenames "*.json")
+;;(add-to-list 'counsel-etags-ignore-filenames "*.html")
+
+;; Don't ask before rereading the TAGS files if they have changed
+(setq tags-revert-without-query t)
+;; Don't warn when TAGS files are large
+(setq large-file-warning-threshold nil)
+;; How many seconds to wait before rerunning tags for auto-update
+(setq counsel-etags-update-interval 180)
+
+;; Set up auto-update
+;; (add-hook
+;;  'prog-mode-hook
+;;  (lambda () (add-hook 'after-save-hook
+;;                       (lambda ()
+;;                         (counsel-etags-virtual-update-tags))))
+;;  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Window numbering
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 已经安装了
+;; M-数字键 就可以切换窗口 比如M-1跳到窗口1上
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; wgrep
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 已经安装了
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Python mode settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 已经安装了 elpy
+
+;; 安装yapfify
+;; 它是用来格式化python的
+(use-package yapfify
+  :ensure t
+  :init
+  (add-hook 'python-mode-hook 'yapf-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Clang-format
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; clang-format can be triggered using C-c C-f
+;; Create clang-format file using google style
+;; clang-format -style=google -dump-config > .clang-format
+(use-package clang-format
+  :ensure t
+  :bind (("C-c C-f" . clang-format-region))
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Modern C++ code highlighting
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package modern-cpp-font-lock
+  :ensure t
+  :config
+  (modern-c++-font-lock-global-mode t)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C++ keys
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package cc-mode
+  :ensure t
+  :config
+  (define-key c++-mode-map (kbd "C-c C-c") 'compile)
+  (define-key c++-mode-map (kbd "C-c C-k") 'kill-compilation)
+  (setq compile-command my:compile-command)
+  (use-package google-c-style
+    :ensure t
+    :config
+    ;; This prevents the extra two spaces in a namespace that Emacs
+    ;; otherwise wants to put... Gawd!
+    (add-hook 'c-mode-common-hook 'google-set-c-style)
+    ;; Autoindent using google style guide
+    (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+    )
+  )
+
+;; Change tab key behavior to insert spaces instead
+(setq-default indent-tabs-mode nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package: ycmd (YouCompleteMeDaemon)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set up YouCompleteMe for emacs:
+;; https://github.com/Valloric/ycmd
+;; https://github.com/abingham/emacs-ycmd
+(defvar my:python-location (executable-find (nth 0 my:ycmd-server-command)))
+(if (not my:python-location)
+    (message
+     "Could not start YouCompleteMeDaemon because the python executable could
+not be found.\nSpecified executable is: '%s'\nPlease set my:ycmd-server-command
+appropriately in ~/.emacs.el.\n" (nth 0 my:ycmd-server-command)))
+(if (not (file-directory-p (nth 1 my:ycmd-server-command)))
+    (message "Could not YouCompleteMeDaemon because the specified directory does
+not exist.\nSpecified directory is: '%s'\nPlease set my:ycmd-server-command
+appropriately in ~/.emacs.el.\n" (nth 1 my:ycmd-server-command)))
+(if (and my:python-location
+         (file-directory-p (nth 1 my:ycmd-server-command)))
+    (use-package ycmd
+      :ensure t
+      :init
+      ;;(add-hook 'after-init-hook #'global-ycmd-mode)
+      (add-hook 'c-mode-common-hook 'ycmd-mode)
+      :config
+      (progn
+        (set-variable 'ycmd-server-command my:ycmd-server-command)
+        (set-variable 'ycmd-extra-conf-whitelist my:ycmd-extra-conf-whitelist)
+        (set-variable 'ycmd-global-config my:ycmd-global-config)
+        (setq ycmd-force-semantic-completion t)
+        (use-package company-ycmd
+          :ensure t
+          :config
+          (company-ycmd-setup)
+          )
+
+        (use-package flycheck-ycmd
+          :ensure t
+          :init
+          (add-hook 'c-mode-common-hook 'flycheck-ycmd-setup)
+          )
+
+        ;; Add displaying the function arguments in mini buffer using El Doc
+        (use-package ycmd-eldoc
+          :init
+          ;; For some reason ycmd-eldoc doesn't work properly in Python mode.
+          ;; I get a "connection refused" error when it connects to JediHTTP
+          (add-hook 'c-mode-common-hook
+                    (lambda ()
+                      (ycmd-eldoc-mode t)))
+          )
+        )
+      )
+  )
+
+;; Set up code completion with company
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package company
+  :ensure t
+  :config
+  ;;在init-company有设置
+  ;;(add-hook 'after-init-hook 'global-company-mode)
+  ;; remove unused backends
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Configure flycheck
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Note: For C++ we use flycheck-ycmd
+(use-package flycheck
+  :ensure t
+  :defer t
+  :config
+  ;; Turn flycheck on everywhere
+  (global-flycheck-mode t)
+  ;; There are issues with company mode and flycheck in terminal mode.
+  ;; This is outlined at:
+  ;; https://github.com/abingham/emacs-ycmd
+  (when (not (display-graphic-p))
+    (setq flycheck-indication-mode nil))
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Magit
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (use-package magit
+;;   :ensure t
+;;   :requires dash
+;;   :after (ivy)
+;;   :bind (("M-g M-s" . magit-status)
+;;          ("M-g M-c" . magit-checkout)
+;;          )
+;;   :config
+;;   (add-hook 'magit-mode-hook (lambda () (setq whitespace-mode -1)))
+;;   (setq magit-completing-read-function 'ivy-completing-read)
+;;   )
+;; (use-package magit-gerrit
+;;   :ensure t
+;;   :after magit
+;;   )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; yaml-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package yaml-mode
+  :ensure t
+  :mode (".yml" ".yaml"))
+
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable which function mode and set the header line to display both the
+;; path and the function we're in
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(which-function-mode t)
+
+;; Remove function from mode bar
+(setq mode-line-misc-info
+      (delete (assoc 'which-func-mode
+                     mode-line-misc-info) mode-line-misc-info))
+
+
+(defmacro with-face
+    (str &rest properties)
+  `(propertize ,str 'face (list ,@properties)))
+
+(defun sl/make-header ()
+  "."
+  (let* ((sl/full-header (abbreviate-file-name buffer-file-name))
+         (sl/header (file-name-directory sl/full-header))
+         (sl/drop-str "[...]")
+         )
+    (if (> (length sl/full-header)
+           (window-body-width))
+        (if (> (length sl/header)
+               (window-body-width))
+            (progn
+              (concat (with-face sl/drop-str
+                                 :background "blue"
+                                 :weight 'bold
+                                 )
+                      (with-face (substring sl/header
+                                            (+ (- (length sl/header)
+                                                  (window-body-width))
+                                               (length sl/drop-str))
+                                            (length sl/header))
+                                 ;; :background "red"
+                                 :weight 'bold
+                                 )))
+          (concat
+           (with-face sl/header
+                      ;; :background "red"
+                      :foreground "red"
+                      :weight 'bold)))
+      (concat (if window-system ;; In the terminal the green is hard to read
+                  (with-face sl/header
+                             ;; :background "green"
+                             ;; :foreground "black"
+                             :weight 'bold
+                             :foreground "#8fb28f"
+                             )
+                (with-face sl/header
+                           ;; :background "green"
+                           ;; :foreground "black"
+                           :weight 'bold
+                           :foreground "blue"
+                           ))
+              (with-face (file-name-nondirectory buffer-file-name)
+                         :weight 'bold
+                         ;; :background "red"
+                         )))))
+
+(defun sl/display-header ()
+  "Create the header string and display it."
+  ;; The dark blue in the header for which-func is terrible to read.
+  ;; However, in the terminal it's quite nice
+  (if window-system
+      (custom-set-faces
+       '(which-func ((t (:foreground "#8fb28f")))))
+    (custom-set-faces
+     '(which-func ((t (:foreground "blue"))))))
+  ;; Set the header line
+  (setq header-line-format
+
+        (list "-"
+              '(which-func-mode ("" which-func-format))
+              '("" ;; invocation-name
+                (:eval (if (buffer-file-name)
+                           (concat "[" (sl/make-header) "]")
+                         "[%b]")))
+              )
+        )
+  )
+;;在which-func-mode增加相应的模式，就可以在headline显示函数[函数][文件路径]
+(setq which-func-modes (append  '(c++-mode
+                                  c-mode
+                                  java-mode
+                                  emacs-lisp-mode
+                                  lisp-mode
+                                  objc-mode
+                                  python-mode
+                                  makefile-mode
+                                  sh-mode)
+                                which-func-modes))
+;; Call the header line update
+(add-hook 'buffer-list-update-hook
+          'sl/display-header)
+
+
+;;let-alist need elpa.gnu.org/packages/
+;;------------------------------------------------------------------------------
+(provide 'init-nilsdeppe)
