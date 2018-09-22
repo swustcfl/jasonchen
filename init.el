55bfafb5dacfef906894ab2dafb54f88ae115ab4
swustcfl <jason.chen@justalk.com>
2018-09-23 06:42:57 +0800

    sync: drop mozrepl support 	33fbbf49

--- init.el
+++ init.el
@@ -116,7 +116,6 @@
   (require-init 'init-gud)
   (require-init 'init-linum-mode)
   ;; (require-init 'init-gist)
-  (require-init 'init-moz)
   (require-init 'init-gtags)
   ;; init-evil dependent on init-clipboard
   (require-init 'init-clipboard)
