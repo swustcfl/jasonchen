7ebca09b221ca3cf41833cdd39c95754504ba74c
jason.chen <jason.chen@juphoon.com.cn>
2020-12-24 10:40:39 +0800

    feature: 删除jc-org-zh-en-aligned-table相关代码

--- lisp/init-jasonchen.el
+++ lisp/init-jasonchen.el
@@ -11,23 +11,6 @@
 ;; 设置字体集和face的想法来自http://zcodes.net/2016/08/22/emacs-notes-config.html
 ;; https://github.com/zcodes/emacs.d/blob/7d15ef5793a5f1d2f89aa3277639aa1ed89a9ff0/lisp/zcodes-org.el
 
-;;这个是单单对org的table产生影响的设置
-;;FIXBUG
-(defun jc-org-zh-en-aligned-table ()
-  (interactive)
-
-  ;;创建字体集合，ascii码使用 source code pro ，中文使用hiragino
-  (setq fontset-orgtable (create-fontset-from-ascii-font "Source Code Pro-14"))
-  (dolist (charset '(kana han cjk-misc bopomofo))
-    (set-fontset-font fontset-orgtable charset
-                      (font-spec :family  "Hiragino Sans GB" :size 16)))
-
-  ;;设置org-table的face属性
-  (set-face-attribute 'org-table nil :font
-                      (format   "%s:pixelsize=%d"  "Source Code Pro"  14)
-                      :fontset fontset-orgtable)
-  )
-
 ;;生效这个org的整个buffer
 (defun jc-org-zh-en-aligned-buffer ()
   (interactive)
